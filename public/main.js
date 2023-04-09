ba96ef96407270aa3139cb6a6644b417d6ff40db
Christian Pearce <cotblvd@gmail.com>
2023-04-09 07:48:12 -0600

    stein tracker

--- public/main.js
+++ public/main.js
@@ -28,3 +28,24 @@ const signUp = () => {
     const emailRegex = /\S+@\S+\.\S+/;
     return emailRegex.test(email);
   }
+
+  // Set initial counter value
+let counter = 0;
+
+// Get span element that displays the counter value
+const counterSpan = document.getElementById('stein-count');
+
+// Define function for incrementing the counter
+function increment() {
+  counter++;
+  counterSpan.textContent = counter;
+}
+
+// Define function for decrementing the counter
+function decrement() {
+  if (counter > 0) {
+    counter--;
+    counterSpan.textContent = counter;
+  }
+}
+
